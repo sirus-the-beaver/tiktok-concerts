38f724bb63f960b115224ca3a64d2c910234a531
sirus-the-beaver <salaris@oregonstate.edu>
2024-06-22 03:22:40 -0700

    Update file structure for backend

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const userRoutes = require('./routes/userRoutes');
-const eventRoutes = require('./routes/eventRoutes');
+const userRoutes = require('./routes/users');
+const eventRoutes = require('./routes/events');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
