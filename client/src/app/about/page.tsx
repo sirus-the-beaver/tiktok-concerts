b38458a1d14d1a58e1c81e1534594e4d43d46be6
sirus-the-beaver <salaris@oregonstate.edu>
2024-06-22 00:47:11 -0700

    Create About page

---
+++ client/src/app/about/page.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function About() {
+    return (
+      <div>
+        <Head>
+          <title>About TikTok Concerts</title>
+          <meta name="description" content="Learn more about TikTok Concerts." />
+        </Head>
+        <main className='container mx-auto px-4'>
+          <h1 className='text-4xl font-bold my-4'>About TikTok Concerts</h1>
+          <p className='text-lg'>Watch your favorite artists perform live on TikTok.</p>
+        </main>
+      </div>
+    );
+  }
