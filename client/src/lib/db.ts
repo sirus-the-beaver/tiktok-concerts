3a195dd3db6a880726cc9e38caa29dbd7ec256a7
sirus-the-beaver <salaris@oregonstate.edu>
2024-07-07 16:31:40 -0700

    Reinstate db connection caching

--- client/src/lib/db.ts
+++ client/src/lib/db.ts
@@ -1,7 +1,7 @@
 import mongoose, { Connection } from 'mongoose';
 
 // Cache the connection to avoid connecting to the database multiple times
-// let cachedConnection: Connection | null = null;
+let cachedConnection: Connection | null = null;
 
 /**
  * Connect to the database.
@@ -9,16 +9,15 @@ import mongoose, { Connection } from 'mongoose';
  * @returns The connection to the database.
  */
 export async function connectToDatabase() {
-    // if (cachedConnection) {
-        // return cachedConnection;
-    // }
+    if (cachedConnection) {
+        return cachedConnection;
+    }
     try {
         const cnx = await mongoose.connect(process.env.MONGODB_URI!);
 
-        // cachedConnection = cnx.connection;
+        cachedConnection = cnx.connection;
 
-        // return cachedConnection;
-        return cnx.connection;
+        return cachedConnection;
     } catch (error) {
         console.log(error);
         throw error;
