4af7b954325a728e903db6aa50a7cd0d0596ffe4
sirus-the-beaver <salaris@oregonstate.edu>
2024-07-07 05:09:56 -0700

    Install and add Vercel web analytics component

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { connectToDatabase } from "@/lib/db";
 import React from "react";
 import ToggleMenu from "@/components/ToggleMenu";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/react';
 
 // Metadata for the layout.
 export const metadata: Metadata = {
@@ -33,6 +34,7 @@ export default async function RootLayout({
           <main className="mb-auto p-4 h-fit">
             {children}
             <SpeedInsights />
+            <Analytics />
           </main>
 
           <footer className="h-10 text-center">
