71b3dfedd1fe819e6bfbeef26a9929cf072c4b52
sirus-the-beaver <salaris@oregonstate.edu>
2024-07-06 19:17:39 -0700

    Wrap useSearchParams in Suspense boundary

--- client/src/app/update/page.tsx
+++ client/src/app/update/page.tsx
@@ -4,22 +4,33 @@ import UpdateEventForm from '@/components/UpdateEventForm';
 import { useRouter, usePathname, useSearchParams } from 'next/navigation';
 
 /**
+ * Search component.
+ * 
+ * @returns {JSX.Element} Search component.
+ */
+function Search() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  const eventId = searchParams.get('id') as string;
+
+  return (
+    <div className='m-4'>
+      <UpdateEventForm eventId={eventId}/>
+    </div>
+  )
+}
+
+/**
  * Update page.
  * 
  * @returns {JSX.Element} Update page.
  */
 export default function Update() {
-    const router = useRouter();
-    const pathname = usePathname();
-    const searchParams = useSearchParams();
-
-    const eventId = searchParams.get('id') as string;
-
     return (
       <Suspense>
-        <div className='m-4'>
-          <UpdateEventForm eventId={eventId}/>
-        </div>
+        <Search />
       </Suspense>
     );
   }
